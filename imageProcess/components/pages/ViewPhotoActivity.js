0e38f42d9f6eac2a4c1caec2e87cb6441a1ba635
matterholt <matterholt.dev@gmail.com>
2021-10-05 23:27:08 -0400

    basic flatlist view of images

--- imageProcess/components/pages/ViewPhotoActivity.js
+++ imageProcess/components/pages/ViewPhotoActivity.js
@@ -1,14 +1,67 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+
 import {
-    StyleSheet,
+    StyleSheet,Text,
+      View,
+      Platform,
+      FlatList,
+      Image,SafeAreaView,
   } from 'react-native';
 
   import {ActionButton,PageLayout}from "../index"
 
-export const ViewPhotoActivity =({navigation})=> {
-    return(
+export const ViewPhotoActivity =({route,navigation})=> {
+  const [selectedImages, setSelectedImages] = useState()
+  const { data } = route.params;
 
+
+
+    useEffect(()=>{
+      const imageList = data.map((x,y)=>{
+        let imgDetail ={id: String(y),
+        contentType: x.mime,
+        fileSize: x.size,
+        filePath: x.path,
+      }
+
+      if (Platform.OS === 'ios') {
+        imgDetail.fileName = x.filename;
+    } else {
+        let path = x.path.split('/');
+        imgDetail.fileName = path[path.length - 1];
+    }
+    
+      return imgDetail
+      })
+      setSelectedImages(imageList)
+      
+
+      
+    },[])
+
+    return(
       <PageLayout>
+        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
+        {/* <Text>itemId: {JSON.stringify(selectedImages)}</Text> */}
+        <SafeAreaView>
+          <FlatList
+            data={selectedImages}
+            renderItem={({item}) =>{ return(
+              <View>
+              <Text>{item.fileName}</Text>
+              <Image
+              style={{    width: 166,
+                height: 158,}}
+                  source={{
+                    uri: item.filePath
+                  }}
+                />
+              </View>
+          )}}
+          keyExtractor={item => item.id}
+          />
+          </SafeAreaView>
+        </View>
         <ActionButton
           textInput ="Back Home"
           toNavigate ="Home"
