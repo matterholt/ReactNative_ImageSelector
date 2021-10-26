ad4d90b5ce2d4c1bd2ef905da4472a5b259b8d0d
matterholt <matterholt.dev@gmail.com>
2021-10-26 00:03:32 -0400

    playing around with nav header

--- textFromImage/App.js
+++ textFromImage/App.js
@@ -11,30 +11,37 @@ import { NativeBaseProvider, Center } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import {HomeScreen,TakePicture, SelectPicture,SearchToxins,AppInfo} from "./Screens/index"
-
-
+import { HomeScreen, TakePicture, SelectPicture, SearchToxins, AppInfo } from './Screens/index';
 
 const Stack = createNativeStackNavigator();
 
-const App = () =>  {
-
+const App = () => {
   return (
     <NavigationContainer>
       <NativeBaseProvider>
-      
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Take Picture" component={TakePicture} />
-        <Stack.Screen name="Select Picture" component={SelectPicture} />
-        <Stack.Screen name="Search Toxins" component={SearchToxins} />
-        <Stack.Screen name="App Info" component={AppInfo} />
-      </Stack.Navigator>
- 
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              title: 'Toxin Finder',
+              headerStyle: {
+                backgroundColor: '#f4511e',
+              },
+              headerTintColor: '#fff',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
+          <Stack.Screen name="Take Picture" component={TakePicture} />
+          <Stack.Screen name="Select Picture" component={SelectPicture} />
+          <Stack.Screen name="Search Toxins" component={SearchToxins} />
+          <Stack.Screen name="App Info" component={AppInfo} />
+        </Stack.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
-  )
+  );
 };
-
 
 export default App;
